908369f47ab832d6533e026f9c63f38afc64f492
lixuming <96899229+lixuming00@users.noreply.github.com>
2022-09-30 09:57:49 +0800

    Update app.config.js

--- src/app.config.js
+++ src/app.config.js
@@ -1,10 +1,6 @@
 export default defineAppConfig({
   pages: [
     'pages/home/index',
-    'pages/devices/index',
-    'pages/profit/index',
-    'pages/welcome/index',
-    'pages/login/index',
   ],
   window: {
     backgroundTextStyle: 'light',
@@ -21,18 +17,6 @@ export default defineAppConfig({
         text: '首页',
         iconPath: `./static/images/home.png`,
         selectedIconPath: './static/images/home_.png'
-      },
-      {
-        pagePath: 'pages/devices/index',
-        text: '设备管理',
-        iconPath: `./static/images/device.png`,
-        selectedIconPath: './static/images/device_.png'
-      },
-      {
-        pagePath: 'pages/profit/index',
-        text: '提现记录',
-        iconPath: `./static/images/list.png`,
-        selectedIconPath: './static/images/list_.png'
       }
     ],
     'color': '#BDBDBD',
